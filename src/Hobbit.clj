e2c4d169ae4460f389df1aacf7eaf8131672844a
Justin Neil <justinmneil@gmail.com>
2016-05-17 16:02:33 -0400

    Added Hobbit namespace

--- src/Hobbit.clj
+++ src/Hobbit.clj
@@ -38,4 +38,26 @@
 
 
 
- (print (symmetrize-body-parts asym-hobbit-body-parts))
+ (defn better-symmetrize-body-parts
+   "Expects a sequence of maps that have a :name and :size"
+   [asym-body-parts]
+   (reduce (fn [final-body-parts part]
+             (into final-body-parts (set [part (matching-part part)])))
+           []
+           asym-body-parts))
+
+ (defn hit
+   [asym-body-parts]
+   (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+         body-part-size-sum (reduce + (map :size sym-parts))
+         target (rand body-part-size-sum)]
+     (loop [[part & remaining] sym-parts
+            accumulated-size (:size part)]
+       (if (> accumulated-size target)
+         part
+         (recur remaining (+ accumulated-size (:size (first remaining))))))))
+
+ (loop [iteration 1]
+   (println (str "Hit " iteration ": " (hit asym-hobbit-body-parts)))
+   (if (< iteration 3)
+      (recur (inc iteration))))
